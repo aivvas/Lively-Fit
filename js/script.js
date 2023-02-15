ea8caa95af63861a5a2e31e7c7faea327a9c2fd5
aivvas <vassi.aiva@gmail.com>
2023-02-15 11:25:17 +0100

    api js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const baseUrl = "http://localhost/lively-fit//wp-json/wp/v2";
+const productContainer = document.getElementById("myDiv")
+  
+  async function getProducts(url){
+      const response = await fetch(url);
+      const products = await response.json();
+      products.forEach(function(product){
+          productContainer.innerHTML += 
+          '<div id="myDiv">'+ product.header +'</div>';
+})
+console.log(products);
+console.log(response);
+}
+getProducts(baseUrl);

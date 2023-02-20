39986f8f319335750c5fb3f4594e945399a15f38
aivvas <vassi.aiva@gmail.com>
2023-02-20 11:45:23 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,5 +42,5 @@ request.open("GET", "http://localhost/lively-fit//wp-json/wp/v2");
 request.send();
 request.onload = () => {
     console.log(request);
-    console.log(request.status);
+    console.log(request.response);
 }

7bddebe811efeee3970d30010cb1d8beb9ca7766
aivvas <vassi.aiva@gmail.com>
2023-02-26 12:43:29 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -60,12 +60,12 @@ function getAPIData(url, targetElement) {
   };
 }
 
-getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=0", document.getElementById("APILevelUP"));
-getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=1", document.getElementById("apiresults"));
-getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=2", document.getElementById("APIMeditation"));
-getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=3", document.getElementById("journeyAPI"));
-getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=4", document.getElementById("spiritualityAPI"));
-getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=5", document.getElementById("aboutAPI"));
+getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=0", APILevel);
+getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=1", APIcontainer);
+getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=2", APIMeditation);
+getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=3", APIJourney);
+getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=4", APISpirituality);
+getAPIData("http://localhost/lively-fit//wp-json/wp/v2/posts?per_post=5", APIAbout);
 
 
 

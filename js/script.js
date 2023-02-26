f32a1dbd07557fab9e7da94c5c1c1bd6af3e15b8
aivvas <vassi.aiva@gmail.com>
2023-02-26 11:33:21 +0100

    a lot of api

--- js/script.js
+++ js/script.js
@@ -1,67 +1,10 @@
-/*fetch('https://localhost/rainy-days/shop/wp-json/v2/shop%27)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
 
-    data.forEach(post => {
-      let title = document.createElement('h2');
-      title.textContent = post.title.rendered;
-      let content = document.createElement('div');
-      content.innerHTML = post.content.rendered;
-      document.getElementById('myDiv').appendChild(title);
-      document.getElementById('myDiv2').appendChild(content);
-    });
-  });
-
- fetch('https://localhost/rainy-days/shop/wp-json/v2/shop%27')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  }); 
-
-const baseUrl = "http://localhost/lively-fit//wp-json/wp/v2";
-const productContainer = document.getElementById("myDiv")
-  
-  async function getProducts(url){
-      const response = await fetch(url);
-      const products = await response.json();
-      products.forEach(function(product){
-          productContainer.innerHTML += 
-          '<div id="myDiv">'+ product.header +'</div>';
-})
-console.log(products);
-console.log(response);
-}
-getProducts(baseUrl);
-
-
-
-
-let request = new XMLHttpRequest
-request.open("GET", "http://localhost/lively-fit//wp-json/wp/v2");
-request.send();
-request.onload = () => {
-    console.log(request);
-    console.log(request.posts);
-}
 
 const APIcontainer = document.getElementById("apiresults")
+const APISpirituality = document.getElementById("spiritualityAPI")
 
 let request = new XMLHttpRequest();
-request.open("GET", "http://localhost/lively-fit//wp-json/wp/v2/posts");
-request.send();
-request.onload = () => {
-    const response = JSON.parse(request.responseText);
-    const reponseitems = response[0];
-    console.log(reponseitems.id);
-    console.log(reponseitems.title);
-    console.log(reponseitems.content);
-}; */
-
-const APIcontainer = document.getElementById("apiresults")
-
-let request = new XMLHttpRequest();
-request.open("GET", "http://localhost/lively-fit//wp-json/wp/v2/posts");
+request.open("GET", "http://localhost/lively-fit//wp-json/wp/v2/pages");
 request.send();
 request.onload = () => {
     const response = JSON.parse(request.responseText);
